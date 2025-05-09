a8a70fa54741bbd69edcefd9ae3acc6d8b039277
baaggii <baaggii@yahoo.com>
2025-05-09 12:06:09 +0800

    🎯 Synced with working canvas config on May 6–7

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/erp/',
   plugins: [react()],
-  root: 'src/client',
-  build: {
-     rollupOptions: {
-    input: 'src/client/index.html'
-  },
-    outDir: '../../../../public_html/erp',
-    emptyOutDir: true,
+ root: 'src/client',
+build: {
+  outDir: '../../../../public_html/erp',
+  emptyOutDir: true
+}
+,
     target: 'esnext',
     manifest: true,
     minify: false,
