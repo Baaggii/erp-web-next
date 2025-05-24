5dcd323064a10e6c700a9e5cf42947f3e96c4020
baaggii <baaggii@yahoo.com>
2025-05-24 13:04:23 +0800

    UI: товч ногоон болгов

---
+++ vite.home.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require('vite')
+const react = require('@vitejs/plugin-react')
+
+module.exports = defineConfig({
+  root: 'src/homepage',               // homepage source folder
+  base: '/',                          // serve at domain root
+  publicDir: '.',                     // include all files in src/homepage (e.g. .htaccess)
+  plugins: [react()],
+  build: {
+    outDir: '../../../../public_html', // publish to public_html/
+    emptyOutDir: false
+  }
+})
