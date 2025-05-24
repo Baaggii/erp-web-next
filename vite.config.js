6161276ebf095db2f94b657631b71f9d8dfa0308
baaggii <baaggii@yahoo.com>
2025-05-24 13:48:53 +0800

    UI: товч ногоон болгов

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   root: 'src/client',
   base: '/erp/',
-  publicDir: '.',
   plugins: [react()],
   build: {
     outDir: '../../../../public_html/erp',
