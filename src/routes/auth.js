1f6125344d025c2c564d134a7daeae97f2938533
baaggii <baaggii@yahoo.com>
2025-05-27 21:03:16 +0800

    UI: товч ногоон болгов

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const db = require('../lib/db')
 
 const JWT_SECRET = process.env.JWT_SECRET || 'devsecret'
