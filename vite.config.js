ae7f2214d24651071edde309577be3247c71e1cb
baaggii <baaggii@yahoo.com>
2025-05-30 10:13:15 +0800

    UI: товч ногоон болгов

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: 'src/client',        // ERP SPA source
-  base: '/erp/',             // Asset base path
-  publicDir: 'src/client',   // Copy all files here (e.g. .htaccess)
+  root: 'src/erp.mgt.mn',        // ERP SPA source
+  base: '/',             // Asset base path
+  publicDir: 'src/erp.mgt.mn',   // Copy all files here (e.g. .htaccess)
   plugins: [react()],
   build: {
     outDir: '../../../../erp.mgt.mn',
