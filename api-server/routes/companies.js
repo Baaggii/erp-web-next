bac87d47cd85b540f97caf9a0872928ff6ceb808
baaggii <baaggii@yahoo.com>
2025-05-30 20:05:23 +0800

    UI: товч ногоон болгов

--- api-server/routes/companies.js
+++ api-server/routes/companies.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import { listCompanies } from '../controllers/companyController.js';
+import { listCompaniesHandler } from '../controllers/companyController.js';
 import { requireAuth } from '../middlewares/auth.js';
+
 const router = express.Router();
-router.get('/', requireAuth, listCompanies);
+router.get('/', requireAuth, listCompaniesHandler);
 export default router;
