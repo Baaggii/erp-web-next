4288d7fed806aac956ef290f7b299de42afce406
baaggii <baaggii@yahoo.com>
2025-05-30 13:15:27 +0800

    UI: товч ногоон болгов

--- src/erp.mgt.mn/main.jsx
+++ src/erp.mgt.mn/main.jsx
@@ -2,6 +2,16 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+```jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+
 // If you have global styles for ERP, import them here. Otherwise, remove this line.
 // import './index.css';
 
