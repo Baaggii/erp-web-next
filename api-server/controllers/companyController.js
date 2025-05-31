3b7b50200ddcadb32ddf672cfcf35c217f3187f0
baaggii <baaggii@yahoo.com>
2025-05-31 16:21:22 +0800

    UI: товч ногоон болгов

--- api-server/controllers/companyController.js
+++ api-server/controllers/companyController.js
@@ -1,4 +1,4 @@
-import { listCompanies } from '../../db/index.js';
+import { listCompanies } from '../../../../db/index.js';
 import { requireAuth } from '../middlewares/auth.js';
 
 export async function listCompaniesHandler(req, res, next) {
