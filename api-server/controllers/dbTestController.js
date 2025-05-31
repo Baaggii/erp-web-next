38366083a4e0d454523c7fe57e9b3b8061f272c3
baaggii <baaggii@yahoo.com>
2025-05-31 16:29:09 +0800

    UI: товч ногоон болгов

--- api-server/controllers/dbTestController.js
+++ api-server/controllers/dbTestController.js
@@ -1,4 +1,4 @@
-import { testConnection } from '../../../../db/index.js';
+import { testConnection } from '../../db/index.js';
 export async function testDb(req, res) {
   const ok = await testConnection();
   res.json({ database: ok ? 'OK' : 'FAIL' });
