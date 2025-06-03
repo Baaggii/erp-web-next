49b12f14120f7bafcc9e1430efb63b5708bc9b90
baaggii <baaggii@yahoo.com>
2025-06-03 13:50:27 +0800

    UI: товч ногоон болгов

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  root: 'src/erp.mgt.mn',        // ERP SPA source
-  base: '/',             // Asset base path
-  publicDir: 'src/erp.mgt.mn',   // Copy all files here (e.g. .htaccess)
-  plugins: [react()],
+
+
+// vite.config.js (relevant excerpt)
+export default {
+  root: './src/erp.mgt.mn',
   build: {
-    outDir: '../../../../erp.mgt.mn',
-    emptyOutDir: true
-  }
-});
+    ooutDir: '../../../../erp.mgt.mn',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: { main: path.resolve(__dirname, 'src/erp.mgt.mn/index.jsx') }
+    }
+  },
+  plugins: [react()],
+};
