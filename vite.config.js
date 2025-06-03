218633c5df8d0884c3f3ee84bfdc0e0ce7a702ce
baaggii <baaggii@yahoo.com>
2025-06-03 14:25:51 +0800

    UI: товч ногоон болгов

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+// Polyfill __dirname in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-// vite.config.js (relevant excerpt)
-export default {
+export default defineConfig({
   root: 'src/erp.mgt.mn',
   base: '/',             // Asset base path
   publicDir: 'src/erp.mgt.mn',   // Copy all files here (e.g. .htaccess)
   build: {
-    ooutDir: '../../../../erp.mgt.mn',
+    outDir: '../../../../erp.mgt.mn',
     emptyOutDir: true,
     rollupOptions: {
       input: { main: path.resolve(__dirname, 'src/erp.mgt.mn/index.jsx') }
     }
   },
   plugins: [react()],
-};
+});
