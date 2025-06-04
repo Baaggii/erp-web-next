fd5d8b025c2dee343c7ad463537871c026d34f7f
Baaggii <45223700+Baaggii@users.noreply.github.com>
2025-06-04 21:07:21 +0800

    Revert "Revert "Revert "Refactor auth flow and assignments"""

--- api-server/controllers/userCompanyController.js
+++ api-server/controllers/userCompanyController.js
@@ -9,9 +9,10 @@ import { requireAuth } from '../middlewares/auth.js';
 
 export async function listAssignments(req, res, next) {
   try {
-    const empid = req.query.empid;
-    const assignments = empid
-      ? await listUserCompanies(empid)
+    const userId = req.query.userId;
+    const assignments = userId
+      ? await listUserCompanies(userId)
+      : await listAllUserCompanies();
     res.json(assignments);
   } catch (err) {
     next(err);
@@ -20,8 +21,8 @@ export async function listAssignments(req, res, next) {
 
 export async function assignCompany(req, res, next) {
   try {
-    const { empid, companyId, role } = req.body;
-    await assignCompanyToUser(empid, companyId, role);
+    const { userId, companyId, empid, role } = req.body;
+    await assignCompanyToUser(userId, companyId, empid, role);
     res.sendStatus(201);
   } catch (err) {
     next(err);
@@ -30,8 +31,8 @@ export async function assignCompany(req, res, next) {
 
 export async function updateAssignment(req, res, next) {
   try {
-    const { empid, companyId, role } = req.body;
-    await updateCompanyAssignment(empid, companyId, role);
+    const { userId, companyId, role } = req.body;
+    await updateCompanyAssignment(userId, companyId, role);
     res.sendStatus(200);
   } catch (err) {
     next(err);
@@ -40,8 +41,8 @@ export async function updateAssignment(req, res, next) {
 
 export async function removeAssignment(req, res, next) {
   try {
-    const { empid, companyId } = req.body;
-    await removeCompanyAssignment(empid, companyId);
+    const { userId, companyId } = req.body;
+    await removeCompanyAssignment(userId, companyId);
     res.sendStatus(204);
   } catch (err) {
     next(err);
