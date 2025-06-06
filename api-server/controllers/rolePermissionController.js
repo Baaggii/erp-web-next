00422da9b08ad43ac2fbcbb174af61382eec3abc
Baaggii <45223700+Baaggii@users.noreply.github.com>
2025-06-06 15:48:20 +0800

    Refine settings menu and sidebar

---
+++ api-server/controllers/rolePermissionController.js
@@ -0,0 +1,27 @@
+import {
+  listRoleModulePermissions,
+  setRoleModulePermission
+} from '../../db/index.js';
+
+export async function listPermissions(req, res, next) {
+  try {
+    const roleId = req.query.roleId;
+    const perms = await listRoleModulePermissions(roleId);
+    res.json(perms);
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function updatePermission(req, res, next) {
+  try {
+    if (req.user.role !== 'admin') {
+      return res.sendStatus(403);
+    }
+    const { roleId, moduleKey, allowed } = req.body;
+    await setRoleModulePermission(roleId, moduleKey, allowed);
+    res.sendStatus(200);
+  } catch (err) {
+    next(err);
+  }
+}
