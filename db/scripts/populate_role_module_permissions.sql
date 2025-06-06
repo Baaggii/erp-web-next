2a45161f2974ca292fcbc4fcd1538fac1c75a4ca
Baaggii <45223700+Baaggii@users.noreply.github.com>
2025-06-06 22:57:34 +0800

    Update populate_role_module_permissions.sql

--- db/scripts/populate_role_module_permissions.sql
+++ db/scripts/populate_role_module_permissions.sql
@@ -1,5 +1,6 @@
 -- Populate role_module_permissions from role_default_modules
-INSERT INTO role_module_permissions (role_id, module_key, allowed)
-SELECT rdm.role_id, rdm.module_key, rdm.allowed
-FROM role_default_modules rdm
+INSERT INTO role_module_permissions (company_id, role_id, module_key, allowed)
+SELECT c.id, rdm.role_id, rdm.module_key, rdm.allowed
+FROM companies c
+CROSS JOIN role_default_modules rdm
 ON DUPLICATE KEY UPDATE allowed = VALUES(allowed);
