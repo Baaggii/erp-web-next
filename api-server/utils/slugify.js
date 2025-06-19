135f33a61ac04250f19d4466232ad1dac4238496
Baaggii <45223700+Baaggii@users.noreply.github.com>
2025-06-19 15:42:40 +0800

    feat: auto-create module for transaction forms

---
+++ api-server/utils/slugify.js
@@ -0,0 +1,9 @@
+export function slugify(text) {
+  return String(text)
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .toLowerCase()
+    .replace(/[^a-z0-9]+/g, '_')
+    .replace(/^_+|_+$/g, '')
+    .slice(0, 64);
+}
