5da5d1405853ddc1a2e83217fa838b9ab714bfb2
Baaggii <45223700+Baaggii@users.noreply.github.com>
2025-06-29 04:04:13 +0800

    Fix config loader order for extra fields

--- src/erp.mgt.mn/pages/CodingTables.jsx
+++ src/erp.mgt.mn/pages/CodingTables.jsx
@@ -1285,21 +1285,24 @@ export default function CodingTablesPage() {
         setIdFilterMode(cfg.idFilterMode || 'contains');
         setIdColumn(cfg.idColumn || '');
         setNameColumn(cfg.nameColumn || '');
+        const extras =
+          cfg.extraFields && cfg.extraFields.length > 0 ? cfg.extraFields : [''];
+        setExtraFields(extras);
         setOtherColumns(cfg.otherColumns || []);
         setUniqueFields(cfg.uniqueFields || []);
         setCalcText(cfg.calcText || '');
         setColumnTypes(cfg.columnTypes || {});
+        if (cfg.columnTypes) {
+          const hdrs = Object.keys(cfg.columnTypes).filter(
+            (h) => !extras.includes(h)
+          );
+          setHeaders(hdrs);
+        }
         setNotNullMap(cfg.notNullMap || {});
         setAllowZeroMap(cfg.allowZeroMap || {});
         setDefaultValues(cfg.defaultValues || {});
         setDefaultFrom(cfg.defaultFrom || {});
         setRenameMap(cfg.renameMap || {});
-        if (cfg.columnTypes) {
-          setHeaders(Object.keys(cfg.columnTypes));
-        }
-        setExtraFields(
-          cfg.extraFields && cfg.extraFields.length > 0 ? cfg.extraFields : ['']
-        );
         setPopulateRange(cfg.populateRange || false);
         setStartYear(cfg.startYear || '');
         setEndYear(cfg.endYear || '');
