eb2b0ad434580c2adc0fad20627ea81d525daef3
Baaggii <45223700+Baaggii@users.noreply.github.com>
2025-08-02 16:15:32 +0800

    Revert "feat: enhance image management UI"

--- src/erp.mgt.mn/windows/ImageManagement.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ImageManagementPage from '../pages/ImageManagement.jsx';
-
-/**
- * Window wrapper for the Image Management page.
- */
-export default function ImageManagement() {
-  return <ImageManagementPage />;
-}
