3abdfc7d3eaf1ab689e311055fadc276571ab025
Baaggii <45223700+Baaggii@users.noreply.github.com>
2025-08-02 16:50:18 +0800

    refactor: limit image management imports to UI

---
+++ src/erp.mgt.mn/windows/ImageManagement.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ImageManagementPage from '../pages/ImageManagement.jsx';
+
+/**
+ * Window wrapper for the Image Management page.
+ */
+export default function ImageManagement() {
+  return <ImageManagementPage />;
+}
