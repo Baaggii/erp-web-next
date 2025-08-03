efdf17187a3f518ccaa7f9b37fee75d0bbdaccd1
Baaggii <45223700+Baaggii@users.noreply.github.com>
2025-08-03 15:36:42 +0800

    Revert "Handle transaction image identifiers"

--- src/erp.mgt.mn/App.jsx
+++ src/erp.mgt.mn/App.jsx
@@ -142,7 +142,7 @@ export default function App() {
               <HashRouter>
           <Routes>
             <Route path="/login" element={<LoginPage />} />
-            <Route path="/*" element={<RequireAuth />}>
+            <Route element={<RequireAuth />}> 
               <Route path="/" element={<ERPLayout />}>{roots.map(renderRoute)}</Route>
               <Route
                 path="/inventory-demo"
