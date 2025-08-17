f711ca6159927c6f1c0232f5909e787a1d7657ff
Baaggii <45223700+Baaggii@users.noreply.github.com>
2025-08-17 23:35:18 +0800

    Revert "Group permission actions by form"

--- db/index.js
+++ db/index.js
@@ -311,10 +311,7 @@ export async function getUserLevelActions(userLevelId) {
         perms.functions = {};
         perms.api = {};
         for (const form of Object.values(forms)) {
-          form.buttons?.forEach((b) => {
-            const key = typeof b === 'string' ? b : b.key;
-            perms.buttons[key] = true;
-          });
+          form.buttons?.forEach((b) => (perms.buttons[b] = true));
           form.functions?.forEach((f) => (perms.functions[f] = true));
           form.api?.forEach((a) => {
             const key = typeof a === 'string' ? a : a.key;
@@ -408,10 +405,7 @@ export async function populateMissingPermissions(allow = false) {
   for (const { module_key } of mods) actions.push(['module_key', module_key]);
   const forms = registry.forms || {};
   for (const form of Object.values(forms)) {
-    form.buttons?.forEach((b) => {
-      const key = typeof b === 'string' ? b : b.key;
-      actions.push(['button', key]);
-    });
+    form.buttons?.forEach((b) => actions.push(['button', b]));
     form.functions?.forEach((f) => actions.push(['function', f]));
     form.api?.forEach((a) => {
       const key = typeof a === 'string' ? a : a.key;
