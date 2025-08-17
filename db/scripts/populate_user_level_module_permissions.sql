eabe98f0e1fdf1991d76b954d2c070082cb06431
Baaggii <45223700+Baaggii@users.noreply.github.com>
2025-08-17 15:38:02 +0800

    Exclude transaction modules from permission population

--- db/scripts/populate_user_level_module_permissions.sql
+++ db/scripts/populate_user_level_module_permissions.sql
@@ -3,9 +3,10 @@ INSERT INTO user_level_permissions (userlevel_id, action, action_key)
 SELECT ul.userlevel_id, 'module_key', m.module_key
   FROM user_levels ul
   CROSS JOIN modules m
-  WHERE NOT EXISTS (
-    SELECT 1 FROM user_level_permissions up
-     WHERE up.userlevel_id = ul.userlevel_id
-       AND up.action = 'module_key'
-       AND up.action_key = m.module_key
-  );
+  WHERE m.module_key NOT LIKE 'transactions\_%'
+    AND NOT EXISTS (
+      SELECT 1 FROM user_level_permissions up
+       WHERE up.userlevel_id = ul.userlevel_id
+         AND up.action = 'module_key'
+         AND up.action_key = m.module_key
+    );
