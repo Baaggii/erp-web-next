51eadfde5f4f5c28174e366dfbc688ba1f20022c
Baaggii <45223700+Baaggii@users.noreply.github.com>
2025-08-21 01:08:35 +0800

    Revert "Refresh module cache after transaction form updates"

--- src/erp.mgt.mn/pages/FormsManagement.jsx
+++ src/erp.mgt.mn/pages/FormsManagement.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useMemo } from 'react';
-import { useModules, refreshModules } from '../hooks/useModules.js';
+import { useModules } from '../hooks/useModules.js';
 import { refreshTxnModules } from '../hooks/useTxnModules.js';
 import { debugLog } from '../utils/debug.js';
 import useGeneralConfig from '../hooks/useGeneralConfig.js';
@@ -439,7 +439,6 @@ export default function FormsManagement() {
       }),
     });
     refreshTxnModules();
-    refreshModules();
     alert('Saved');
     if (!names.includes(name)) setNames((n) => [...n, name]);
   }
@@ -452,7 +451,6 @@ export default function FormsManagement() {
       credentials: 'include',
     });
     refreshTxnModules();
-    refreshModules();
     setNames((n) => n.filter((x) => x !== name));
     setName('');
     setConfig({
