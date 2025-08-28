7380bbc1a80fba6a4e5f80c9920c67540dbea7c0
Baaggii <45223700+Baaggii@users.noreply.github.com>
2025-08-28 22:00:06 +0800

    chore: remove redundant company id column

--- db/mgtmn_erp_db.sql
+++ db/mgtmn_erp_db.sql
@@ -1743,8 +1743,7 @@ CREATE TABLE `code_workplace_other` (
 CREATE TABLE `companies` (
   `id` int NOT NULL,
   `name` varchar(100) NOT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `company_id` int NOT NULL DEFAULT '0'
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
