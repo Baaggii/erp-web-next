af99a530b0d55a712d31eeb4d433b6292ec96159
Baaggii <45223700+Baaggii@users.noreply.github.com>
2025-08-28 22:46:03 +0800

    feat: add company creation endpoint

--- api-server/controllers/companyController.js
+++ api-server/controllers/companyController.js
@@ -1,10 +1,29 @@
-import { listCompanies } from '../../db/index.js';
-import { requireAuth } from '../middlewares/auth.js';
+import {
+  listCompanies,
+  insertTableRow,
+  getEmploymentSession,
+} from '../../db/index.js';
 
 export async function listCompaniesHandler(req, res, next) {
   try {
     const companies = await listCompanies();
     res.json(companies);
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function createCompanyHandler(req, res, next) {
+  try {
+    res.locals.logTable = 'companies';
+    const { seedTables = null, ...company } = req.body || {};
+    const session =
+      req.session ||
+      (await getEmploymentSession(req.user.empid, req.user.companyId));
+    if (!session?.permissions?.system_settings) return res.sendStatus(403);
+    const result = await insertTableRow('companies', company, seedTables);
+    res.locals.insertId = result?.id;
+    res.status(201).json(result);
   } catch (err) {
     next(err);
   }
