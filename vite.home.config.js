6055818d5d1c49c34abc00b2a4b295b516bc6f8f
Baaggii <45223700+Baaggii@users.noreply.github.com>
2025-09-12 17:07:38 +0800

    Update vite.home.config.js

--- vite.home.config.js
+++ vite.home.config.js
@@ -1,24 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// vite.home.config.js
 export default defineConfig({
   base: '/',
   root: 'src/homepage',
   publicDir: 'src/homepage',
   plugins: [react()],
-  optimizeDeps: { disabled: true } (skip esbuild pre-bundle),
+  optimizeDeps: { disabled: true }, // skip esbuild pre-bundle
+  esbuild: { legalComments: 'none' },
   build: {
     outDir: '../../../../public_html',
     emptyOutDir: false,
-    minify: false, 
-    cssMinify: false, 
-    target: 'es2020', 
-    rollupOptions: {
-      external: ['sharp', 'puppeteer']
-    },
-  },
-  esbuild: {
-    legalComments: 'none'
+    target: 'es2020',
+    minify: false,
+    cssMinify: false
   }
 })
