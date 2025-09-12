8d9778df7e61a89df80091f96162a6c588bf45f6
Baaggii <45223700+Baaggii@users.noreply.github.com>
2025-09-12 14:51:09 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// vite.config.js
 export default defineConfig({
-  root: 'src/erp.mgt.mn',        // ERP SPA source
-  base: '/',             // Asset base path
-  publicDir: 'src/erp.mgt.mn',   // Copy all files here (e.g. .htaccess)
+  root: 'src/erp.mgt.mn',
+  base: '/',
+  publicDir: 'src/erp.mgt.mn',
   plugins: [react()],
   build: {
     outDir: '../../../../erp.mgt.mn',
     emptyOutDir: true,
-    minify: false
+    minify: false,
+    rollupOptions: {
+      external: ['sharp', 'puppeteer']
+    }
   }
-});
+})
