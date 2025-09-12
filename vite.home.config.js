cb126465e7b39667fe53a8ebd5ba01891d1033c8
Baaggii <45223700+Baaggii@users.noreply.github.com>
2025-09-12 14:49:23 +0800

    Update vite.home.config.js

--- vite.home.config.js
+++ vite.home.config.js
@@ -1,13 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// vite.home.config.js
 export default defineConfig({
   base: '/',
   root: 'src/homepage',
-  publicDir: 'src/homepage',    // ensure .htaccess, index.html/css live here
+  publicDir: 'src/homepage',
   plugins: [react()],
   build: {
     outDir: '../../../../public_html',
-    emptyOutDir: false
+    emptyOutDir: false,
+    minify: false      // disable esbuild minification
+  },
+  esbuild: {
+    legalComments: 'none'
   }
-});
+})
