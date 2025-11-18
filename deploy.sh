4eb9205431baa895be100e07a211f22d2ed87163
Baaggii <45223700+Baaggii@users.noreply.github.com>
2025-11-18 14:19:57 +0800

    Add deployment script to manage builds and environment

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+rm -rf node_modules
+npm install -g npm@11.6.2
+npm run build:homepage
+npm run build:erp
+cp src/erp.mgt.mn/.htaccess ~/erp.mgt.mn/.htaccess
+cp src/homepage/.htaccess ~/public_html/.htaccess
+# npm prune --omit=dev
+pm2 restart erp-app --update-env
