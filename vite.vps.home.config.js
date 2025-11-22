6416d981ac720c184f88a7ee8f0fdfe1e0b32efe
Baaggii <45223700+Baaggii@users.noreply.github.com>
2025-11-22 17:17:06 +0800

    Update Vite config to use path resolution

--- vite.vps.home.config.js
+++ vite.vps.home.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  root: resolve(__dirname, 'src/homepage'),
   build: {
-    outDir: "dist-home",
+    outDir: resolve(__dirname, 'dist-home'),
     emptyOutDir: true,
   },
 });
