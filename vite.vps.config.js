75669a4f9c40b67d57ea6baead55acf0f9bdbe34
Baaggii <45223700+Baaggii@users.noreply.github.com>
2025-11-22 17:16:43 +0800

    Update vite.vps.config.js

--- vite.vps.config.js
+++ vite.vps.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  root: resolve(__dirname, 'src/erp.mgt.mn'),
   build: {
-    outDir: "dist-erp",
+    outDir: resolve(__dirname, 'dist-erp'),
     emptyOutDir: true,
   },
 });
