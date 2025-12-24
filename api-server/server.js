ce2412a9271045b0523aff59bfb48f77a23d8c4d
Baaggii <45223700+Baaggii@users.noreply.github.com>
2025-12-24 21:33:35 +0800

    Normalize display field config handling

--- api-server/server.js
+++ api-server/server.js
@@ -40,6 +40,7 @@ import transactionImageRoutes from "./routes/transaction_images.js";
 import transactionTemporaryRoutes from "./routes/transaction_temporaries.js";
 import aiInventoryRoutes from "./routes/ai_inventory.js";
 import { getGeneralConfig } from "./services/generalConfig.js";
+import { initializeDisplayFieldConfig } from "./services/displayFieldConfig.js";
 import procedureRoutes from "./routes/procedures.js";
 import procTriggerRoutes from "./routes/proc_triggers.js";
 import reportProcedureRoutes from "./routes/report_procedures.js";
@@ -112,6 +113,7 @@ app.set("io", io);
 
 // Serve uploaded images statically
 const { config: imgCfg } = await getGeneralConfig();
+await initializeDisplayFieldConfig();
 const imgBase = imgCfg.images?.basePath || "uploads";
 const projectRoot = path.resolve(__dirname, "../");
 const uploadsRoot = path.isAbsolute(imgBase)
