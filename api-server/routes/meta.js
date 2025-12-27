fbf4452b3e1611b74404cb32d8b804ec12fa3308
Baaggii <45223700+Baaggii@users.noreply.github.com>
2025-12-28 02:58:05 +0800

    Revert "Support JSON array relations in forms and tables"

--- api-server/routes/meta.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getTableMeta } from '../controllers/tableController.js';
-import { requireAuth } from '../middlewares/auth.js';
-
-const router = express.Router();
-
-router.get('/', requireAuth, getTableMeta);
-
-export default router;
