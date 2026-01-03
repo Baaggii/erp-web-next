0a60490cd53d04a05bc364b2f7ca916d308ad41a
Baaggii <45223700+Baaggii@users.noreply.github.com>
2026-01-04 06:03:43 +0800

    Revert "Fix generated column parser for function calls"

--- src/erp.mgt.mn/utils/generatedColumns.js
+++ src/erp.mgt.mn/utils/generatedColumns.js
@@ -479,12 +479,11 @@ class MySqlExpressionParser {
     }
     if (token.type === 'identifier') {
       this.consume();
-      const next = this.peek();
-      const hasOpeningParen =
-        (next?.type === 'paren' && next.value === '(') ||
-        (next?.type === 'operator' && next.value === '(');
-      if (hasOpeningParen) {
-        this.consume();
+      if ((this.peek()?.type === 'paren' && this.peek().value === '(') || this.matchOperator('(')) {
+        if (!(this.peek()?.type === 'paren' && this.peek().value === '(')) {
+          this.index -= 1;
+          this.consume();
+        }
         const args = [];
         if (this.matchOperator(')') || (this.peek()?.type === 'paren' && this.peek().value === ')')) {
           if (this.peek()?.type === 'paren' && this.peek().value === ')') this.consume();
