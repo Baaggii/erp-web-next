3fb9c572782bd4da1e11b5c9a8199c5a4002fbde
Baaggii <45223700+Baaggii@users.noreply.github.com>
2026-01-17 02:17:41 +0800

    Add CNC processing route to main server

--- api-server/server.js
+++ api-server/server.js
@@ -62,6 +62,7 @@ import manualTranslationsRoutes from "./routes/manual_translations.js";
 import posApiEndpointRoutes from "./routes/posapi_endpoints.js";
 import posApiProxyRoutes from "./routes/posapi_proxy.js";
 import posApiReferenceCodeRoutes from "./routes/posapi_reference_codes.js";
+import cncProcessingRoutes from "./routes/cnc_processing.js";
 
 // Polyfill for __dirname in ES modules
 const __filename = fileURLToPath(import.meta.url);
@@ -202,6 +203,7 @@ app.use("/api/translations", translationRoutes);
 app.use("/api/posapi/endpoints", posApiEndpointRoutes);
 app.use("/api/posapi/proxy", posApiProxyRoutes);
 app.use("/api/posapi/reference-codes", posApiReferenceCodeRoutes);
+app.use("/api/cnc_processing", cncProcessingRoutes);
 
 // Serve static React build and fallback to index.html
 const buildDir = path.resolve(__dirname, "../../../erp.mgt.mn");
