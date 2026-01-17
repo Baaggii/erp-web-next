b667ec43145da276a99a64f614558101dc2a1a80
Baaggii <45223700+Baaggii@users.noreply.github.com>
2026-01-17 18:53:57 +0800

    Revert "Fix receipt print preview sizing and preview window dimensions"

--- src/erp.mgt.mn/components/TableManager.jsx
+++ src/erp.mgt.mn/components/TableManager.jsx
@@ -6936,7 +6936,7 @@ const TableManager = forwardRef(function TableManager({
           body: JSON.stringify({ printerId: userSettings.printerId, content: html }),
         }).catch((err) => console.error('Print failed', err));
       } else {
-        const w = window.open('', '_blank', windowFeatures);
+        const w = window.open('', '_blank');
         if (!w) return;
         w.document.write(html);
         w.document.close();
