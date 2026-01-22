cf6a266c4a7d55adb4ce7dc58e1c5357aa55cc80
Baaggii <45223700+Baaggii@users.noreply.github.com>
2026-01-22 17:58:19 +0800

    Fix signature field printing selection

--- src/erp.mgt.mn/components/RowFormModal.jsx
+++ src/erp.mgt.mn/components/RowFormModal.jsx
@@ -4530,7 +4530,8 @@ const RowFormModal = function RowFormModal({
     const all = [...headerCols, ...mainCols, ...footerCols, ...signatureFields];
     const buildSection = (mode) => {
       const list = mode === 'emp' ? printEmpField : printCustField;
-      const allowed = new Set(list.length > 0 ? list : all);
+      const allowedBase = list.length > 0 ? list : all;
+      const allowed = new Set([...allowedBase, ...signatureFields]);
       const signatureCols = signatureFields.filter((c) => allowed.has(c));
       const h = headerCols.filter((c) => allowed.has(c));
       const m = mainCols.filter((c) => allowed.has(c));
