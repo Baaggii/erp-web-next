a2d8350042648c9185afbcfedc4499d5ce3f33f2
Baaggii <45223700+Baaggii@users.noreply.github.com>
2026-01-23 10:08:03 +0800

    Revert "Set default print copies to two"

--- src/erp.mgt.mn/components/TableManager.jsx
+++ src/erp.mgt.mn/components/TableManager.jsx
@@ -577,7 +577,7 @@ const TableManager = forwardRef(function TableManager({
   const [printModalOpen, setPrintModalOpen] = useState(false);
   const [printEmpSelected, setPrintEmpSelected] = useState(true);
   const [printCustSelected, setPrintCustSelected] = useState(false);
-  const [printCopies, setPrintCopies] = useState('2');
+  const [printCopies, setPrintCopies] = useState('1');
   const [printPayload, setPrintPayload] = useState(null);
   const [localRefresh, setLocalRefresh] = useState(0);
   const [procTriggers, setProcTriggers] = useState({});
@@ -6798,7 +6798,7 @@ const TableManager = forwardRef(function TableManager({
       setPrintPayload(normalizedPayload);
       setPrintEmpSelected(true);
       setPrintCustSelected(false);
-      setPrintCopies('2');
+      setPrintCopies('1');
       setPrintModalOpen(true);
     },
     [buildPrintPayloadFromRows, formConfig?.posApiEnabled],
@@ -6816,7 +6816,7 @@ const TableManager = forwardRef(function TableManager({
       setPrintPayload(normalizedPayload);
       setPrintEmpSelected(true);
       setPrintCustSelected(false);
-      setPrintCopies('2');
+      setPrintCopies('1');
       setPrintModalOpen(true);
     },
     [buildPrintPayloadFromRow, editing, formConfig?.posApiEnabled, gridRows],
