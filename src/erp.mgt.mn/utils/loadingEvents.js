a94423dd933ad340dc85c2cf9ee344d228238707
Baaggii <45223700+Baaggii@users.noreply.github.com>
2026-02-10 11:45:43 +0800

    Narrow loader reset scope when closing tabs

--- src/erp.mgt.mn/utils/loadingEvents.js
+++ src/erp.mgt.mn/utils/loadingEvents.js
@@ -6,6 +6,10 @@ export function dispatchEnd(key) {
   window.dispatchEvent(new CustomEvent('loading:end', { detail: { key } }));
 }
 
+export function dispatchReset(key) {
+  window.dispatchEvent(new CustomEvent('loading:reset', { detail: { key } }));
+}
+
 export function currentLoaderKey() {
   return window.__activeTabKey || 'global';
 }
